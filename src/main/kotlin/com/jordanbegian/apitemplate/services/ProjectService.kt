7560fb3e4b62fa0d19174c469e5830c4a58eb24f
Jordan Begian <46720056+jordanbegian@users.noreply.github.com>
2023-02-23 20:26:24 -0500

    establish service layer for project

--- src/main/kotlin/com/jordanbegian/apitemplate/services/ProjectService.kt
+++ src/main/kotlin/com/jordanbegian/apitemplate/services/ProjectService.kt
@@ -1,4 +1,15 @@
 package com.jordanbegian.apitemplate.services
 
+import com.jordanbegian.apitemplate.models.ProjectData
+import com.jordanbegian.apitemplate.models.ProjectRequest
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import java.util.*
+
+@Component
 class ProjectService {
+    fun submitRequest(request: ProjectRequest): Mono<ProjectData> {
+        return ProjectData(UUID.randomUUID().toString(), request.field).toMono()
+    }
 }
