634c951149b1b3dd49356e165572edbb05069a5a
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2023-03-22 14:47:47 -0400

    update the submitRequest function to interact with the repository layer

--- src/main/kotlin/com/jordanbegian/apitemplate/services/ProjectService.kt
+++ src/main/kotlin/com/jordanbegian/apitemplate/services/ProjectService.kt
@@ -1,15 +1,13 @@
 package com.jordanbegian.apitemplate.services
 
 import com.jordanbegian.apitemplate.models.ProjectData
-import com.jordanbegian.apitemplate.models.ProjectRequest
+import com.jordanbegian.apitemplate.repository.ProjectRepository
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
-import java.util.*
 
 @Component
-class ProjectService {
-    fun submitRequest(request: ProjectRequest): Mono<ProjectData> {
-        return ProjectData(UUID.randomUUID().toString(), request.field).toMono()
-    }
+class ProjectService(
+    private val projectRepository: ProjectRepository
+) {
+    fun submitRequest(request: ProjectData): Mono<ProjectData> = projectRepository.submit(request)
 }
