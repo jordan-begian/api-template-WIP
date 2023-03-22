23cbd673d6c202b6d4902037def167c143a6de42
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2023-03-22 14:46:59 -0400

    update repository layer to work with reactive mongo template

--- src/main/kotlin/com/jordanbegian/apitemplate/repository/projectRepository.kt
+++ src/main/kotlin/com/jordanbegian/apitemplate/repository/projectRepository.kt
@@ -1,4 +1,16 @@
 package com.jordanbegian.apitemplate.repository
 
-class projectRepository {
+import com.jordanbegian.apitemplate.models.ProjectData
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+
+@Repository
+class ProjectRepository(@Autowired val template: ReactiveMongoTemplate){
+
+    fun submit(projectData: ProjectData): Mono<ProjectData> {
+        return template.insert(projectData)
+    }
 }
+
